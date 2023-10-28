7f3c816d46449a5f94a5a8f4ec3818011b3ddbd4
mauriander <andermattenmauricio@gmail.com>
2023-10-28 02:24:30 -0300

    UPDATED pop up de maps

--- src/components/SelectorRuta.js
+++ src/components/SelectorRuta.js
@@ -1,65 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 function SelectorRuta({ onRouteChange }) {
   const [transportData, setTransportData] = useState([]);
-  const [selectedRoute, setSelectedRoute] = useState('');
+  const [selectedRoute, setSelectedRoute] = useState("");
   const [loading, setLoading] = useState(true);
 
-  const CLIENT_ID = 'cb6b18c84b3b484d98018a791577af52';
-  const CLIENT_SECRET = '3e3DB105Fbf642Bf88d5eeB8783EE1E6';
+  const CLIENT_ID = "cb6b18c84b3b484d98018a791577af52";
+  const CLIENT_SECRET = "3e3DB105Fbf642Bf88d5eeB8783EE1E6";
 
-const Divisor = styled.div`
-margin-top:6px
-  
-`;
+  const Divisor = styled.div`
+    margin-top: 6px;
+  `;
   useEffect(() => {
     setLoading(true); // Set loading to true while waiting for data
 
-    fetch(`https://datosabiertos-transporte-apis.buenosaires.gob.ar/colectivos/vehiclePositionsSimple?&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`)
+    fetch(
+      `https://datosabiertos-transporte-apis.buenosaires.gob.ar/colectivos/vehiclePositionsSimple?&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+    )
       .then((response) => response.json())
       .then((data) => {
         setTransportData(data);
-        setLoading(false); 
+        setLoading(false);
       })
       .catch((error) => {
         console.error(error);
-        setLoading(false); 
+        setLoading(false);
       });
   }, []);
 
-   const handleRouteChange = (event) => {
-    const selectedRouteID = transportData.find((item) => item.route_short_name === event.target.value)?.route_id;
+  const handleRouteChange = (event) => {
+    const selectedRouteID = transportData.find(
+      (item) => item.route_short_name === event.target.value
+    )?.route_id;
     setSelectedRoute({ name: event.target.value, id: selectedRouteID });
     onRouteChange(selectedRouteID); // Pass the selected route ID to the parent component
-  }; 
-
+  };
 
   return (
-    
     <Divisor>
-  <select id="routeSelector" onChange={handleRouteChange} style={{ background: 'gray' }}>
-    <option value="">Selecciona una ruta</option>
-    {loading ? (
-      <option value="" disabled>
-        LOADING...
-      </option>
-    ) : (
-      [...new Set(transportData.map(item => item.route_short_name))].sort().map((routeName, index) => (
-        <option key={index} value={routeName}>
-          {routeName} {transportData.find(item => item.route_short_name === routeName)?.trip_headsign}
-        </option>
-      ))
-    )}
-  </select>
-  {selectedRoute && (
-    <p>
-      Ruta seleccionada: {selectedRoute.name}
-    </p>
-  )}
-</Divisor>
-
-      
- 
+      <select
+        id="routeSelector"
+        onChange={handleRouteChange}
+        style={{ background: "gray" }}
+      >
+        <option value="">Selecciona una ruta</option>
+        {loading ? (
+          <option value="" disabled>
+            LOADING...
+          </option>
+        ) : (
+          [...new Set(transportData.map((item) => item.route_short_name))]
+            .sort()
+            .map((routeName, index) => (
+              <option key={index} value={routeName}>
+                {routeName}{" "}
+                {
+                  transportData.find(
+                    (item) => item.route_short_name === routeName
+                  )?.trip_headsign
+                }
+              </option>
+            ))
+        )}
+      </select>
+      {selectedRoute && <p>Ruta seleccionada: {selectedRoute.name}</p>}
+    </Divisor>
   );
 }
 
--- src/components/Transport.js
+++ src/components/Transport.js
@@ -1,16 +1,16 @@
-import React, {useState,useEffect } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, Icon } from 'react-leaflet';
-import SelectorRuta from './SelectorRuta';
-import {  FaClock,FaBus, FaHeart} from 'react-icons/fa';
-import { BsHeartFill, BsHeart } from "react-icons/bs";
-import L from 'leaflet';
+import React, { useState, useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup, Icon } from "react-leaflet";
+import SelectorRuta from "./SelectorRuta";
+import { FaClock, FaBus, FaHeart, FaInfo, FaRoad, FaMarker, FaDirections, FaTachometerAlt, FaMapMarker, FaMapMarked } from "react-icons/fa";
+
+import L from "leaflet";
 
 function Transport({ ruta }) {
   const [transportData, setTransportData] = useState([]);
 
-  const CLIENT_ID = 'cb6b18c84b3b484d98018a791577af52';
-  const CLIENT_SECRET = '3e3DB105Fbf642Bf88d5eeB8783EE1E6';
-  const ROUTE_ID = '1468';
+  const CLIENT_ID = "cb6b18c84b3b484d98018a791577af52";
+  const CLIENT_SECRET = "3e3DB105Fbf642Bf88d5eeB8783EE1E6";
+  const ROUTE_ID = "1468";
 
   const fetchTransportData = () => {
     fetch(
@@ -19,7 +19,7 @@ function Transport({ ruta }) {
       .then((response) => response.json())
       .then((datat) => {
         setTransportData(datat);
-        console.log('datat ' + datat[0]);
+        console.log("datat " + datat[0]);
       })
       .catch((error) => {
         console.error(error);
@@ -31,41 +31,52 @@ function Transport({ ruta }) {
 
     const interval = setInterval(() => {
       fetchTransportData();
-    },31000);
+    }, 31000);
 
     return () => {
       clearInterval(interval);
     };
   }, [ruta]);
 
-   const busIcon = new L.divIcon({
-    className: 'custom-icon',
-    html: `<div>${<BsHeart />}</div>`,
+  const busIcon = new L.divIcon({
+    className: "custom-icon",
+    html: `<div>${(<FaBus></FaBus>)}</div>`,
     iconSize: [25, 41],
     iconAnchor: [12, 41],
   });
 
   return (
-    <MapContainer center={[-34.60376, -58.38162]} zoom={10} scrollWheelZoom={false} style={{ height: '90vh', margin: '2px' }}>
+    <MapContainer
+      center={[-34.60376, -58.38162]}
+      zoom={10}
+      scrollWheelZoom={false}
+      style={{ height: "90vh", margin: "2px" }}
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
       {transportData.map((data, index) => (
-        <Marker 
-     key={index} position={[data.latitude, data.longitude]} >
+        <Marker key={index} position={[data.latitude, data.longitude]}>
           <Popup>
+          
             {/* Puedes personalizar la información del marcador aquí utilizando los datos */}
             <div>
-              <p>Información adicional:</p>
-              <p>Ruta: {data.route_short_name}</p>
-              <p>Velocidad: {data.speed}</p>
-              <p>IDA 1 / VUELTA 0: {data.direction}</p>
-              <p> Empresa: {data.agency_name}</p>
-              <p>Destino: {data.trip_headsign}</p>
-              <p>ID: {data.id}</p>
-              {[data.latitude, data.longitude]}
+              <span><FaInfo/> Información:</span>
+             <p> <span><FaRoad></FaRoad> Ruta: {data.route_short_name}</span></p>
+              <p><FaTachometerAlt></FaTachometerAlt>  Velocidad: {data.speed.toFixed(2)}</p>
+              <p><FaDirections></FaDirections> Dirección: {data.direction === "1" ? "IDA" : "VUELTA"}</p>
+              <p> <FaBus/> Empresa: {data.agency_name}</p>
+              <p> <FaMapMarker/> Destino: {data.trip_headsign}</p>
+
+             <a
+  href={`https://www.bing.com/maps?cp=${data.latitude}~${data.longitude}&lvl=17.0`}
+  target="_blank"
+  rel="noopener noreferrer"
+>
+<FaMapMarked/>  Abrir Mapa
+</a>
             </div>
           </Popup>
         </Marker>
